fd901ab676e3b5de5fe5f966a55e0712e93198ac
DeMoManique <frederik.hvarregaard.andersen@gmail.com>
2023-04-17 15:43:05 +0200

    test

--- src/main/java/application/HelloFX.java
+++ src/main/java/application/HelloFX.java
@@ -12,7 +12,7 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, Java " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Label l = new Label("Hello, hej " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
