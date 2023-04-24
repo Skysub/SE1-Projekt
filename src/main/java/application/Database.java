221b699abc6cf7b42e9a5478967c80121b40e47f
Freddy <frederik.hedeandersen@gmail.com>
2023-04-24 15:27:15 +0200

    Update Database.java

--- src/main/java/application/Database.java
+++ src/main/java/application/Database.java
@@ -1,8 +1,9 @@
 package application;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class DataBase {
+public class Database implements Serializable {
     ArrayList<Employee> employees = new ArrayList<>();
     ArrayList<Project> projects = new ArrayList<>();
 
