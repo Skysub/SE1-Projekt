bb20393a206b4a979ab1800ec7a0973220e1e72d
Freddy <frederik.hedeandersen@gmail.com>
2023-04-24 16:52:25 +0200

    Udviddet check for serializability, samt mere serialization implementeret

--- src/main/java/application/Project.java
+++ src/main/java/application/Project.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Project {
-    ArrayList<Activity> activities = new ArrayList<>();
+public class Project implements Serializable{
+	private static final long serialVersionUID = 6537227987620711347L;
+	
+	ArrayList<Activity> activities = new ArrayList<>();
     ArrayList<Employee> employees = new ArrayList<>();
     Employee projectManager = null;
     int ID;
