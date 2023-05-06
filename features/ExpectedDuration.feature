5524fbbb25ab62c00cdbf22a3fd95e1b690dedf6
AbdHaseeb03 <Haseeb2003@outlook.dk>
2023-05-06 17:22:50 +0200

    expected duration cucumber feature og steps

---
+++ features/ExpectedDuration.feature
@@ -0,0 +1,19 @@
+Feature: Work distribution
+Description: Employees should be able to work on up to 20 activities during a week
+Actors: Employee
+
+Scenario: employee can have 20 activities at once
+	Given an employee with the ID "JJCB" exists
+	And 20 activities with start week 5 and end week 6 in the project with the ID 23001 exist
+	And employees can be assigned a maximum of 20 activities per week
+	And the employee is assigned the 19 activities
+	When an employee with the ID "JJCB" is assigned the activity named "19" in the project with ID 23001
+	Then the employee with initials "JJCB" is assigned to all the 20 activities of the project with ID 23001
+
+Scenario: employee cannot have more than 20 activities at once
+	Given an employee with the ID "JJCB" exists
+	And 21 activities with start week 5 and end week 6 in the project with the ID 23001 exist
+	And employees can be assigned a maximum of 20 activities per week
+	And the employee is assigned the 20 activities
+	When an employee with the ID "JJCB" is assigned the activity named "20" in the project with ID 23001
+	Then the error message "Employees cannot be assigned more than 20 activities in a given week" is given
---
+++ src/test/java/example/cucumber/ExpectedDurationSteps.java
@@ -0,0 +1,54 @@
+package example.cucumber;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+
+import application.Database;
+import application.Employee;
+import application.IllegalOperationException;
+import application.Project;
+import application.WorkActivity;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ExpectedDurationSteps {
+	
+	Database database;
+	ErrorMessageHolder errorMessageHolder;
+	Employee recentManager;
+	WorkActivity recentWorkActivity;
+	Project recentProject;
+	
+	public ExpectedDurationSteps(Database database, ErrorMessageHolder errorMessageHolder) {
+		this.database = database;
+		this.errorMessageHolder = errorMessageHolder;
+	}
+	
+
+
+
+@When("the project manager registers {int} hours expected duration on the activity with the name {string} in the project with ID {int}")
+public void projectManagerRegistersExpectedDurationOnTheActivityWithTheNameInTheProjectWithID(String initials, Integer hours, String name,
+		Integer projectID, String managerID) {
+	recentWorkActivity = new WorkActivity(name, database.CreateProject(23001));
+	recentManager = database.getProject(projectID).setManager(database.getEmployee(managerID));
+try {
+	recentWorkActivity.RegisterTime(recentManager, (float) hours, LocalDate.now());
+} catch (IllegalOperationException e) {
+	errorMessageHolder.setErrorMessage(e.getMessage());
+}
+}
+
+@Then("the activity has {int} hours of expected duration registered ")
+public void theActivityHasHoursOfExpectedDuration(Integer hours, String managerID) {
+	assertEquals((float) hours, recentWorkActivity.getTimeRegistered(database.getEmployee(managerID)), 0.0166f);
+	// Checks if the amount worked is accurate to within one minute
+}
+
+
+}
+
+
